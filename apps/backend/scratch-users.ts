e909c491c70cc27eba0dbb0634c5a6b64b730f93
Lalit Kumar <lalitbgs2584@gmail.com>
2026-08-23 01:27:20 +0530

    chore: remove unused file and associated references

--- apps/backend/scratch-users.ts
+++
@@ -1,4 +0,0 @@
-import { prisma } from "db/client";
-const users = await prisma.user.findMany({ select: { id: true, image: true } });
-console.log(JSON.stringify(users));
-await prisma.$disconnect();
